4e82181606bd45e7e9f73e9f6dcfa80c935627b7
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-08-18 16:23:32 +0300

    Done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
+    sentences=0
+    puntuation=[".", "?","!"]
+
+    self.chars.each.with_index do |char,index|
+
+      if puntuation.include?(char) && !puntuation.include?(self[index + 1])
+        sentences +=1
+
+      end
 
   end
+  sentences
+end
 end
